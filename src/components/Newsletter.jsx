d0982b4846557a0ef613fd605ae692a5688d567d
ahsanul0185 <ahsanul0185@gmail.com>
2024-12-18 23:06:41 +0600

    completed full website

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Newsletter = () => {
+  return (
+    <div className="flex flex-col lg:flex-row gap-8 justify-between md:items-center">
+          <div className="text-xl sm:text-2xl text-white">
+            <p className="font-bold">Subscribe to our newsletter</p>
+            <p>latest news, articles, and resources </p>
+          </div>
+          <div className="flex gap-4">
+            <input className="rounded-md w-full md:w-96 px-6 outline-none" type="text" placeholder="Enter your email" />
+
+            <button className="px-8 sm:px-16 py-2 md:py-4 bg-secondary rounded-md text-xl font-bold text-white cursor-pointer">
+              Subscribe
+            </button>
+          </div>
+        </div>
+  )
+}
+
+export default Newsletter
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,12 +1,84 @@
-import React from 'react'
+import React from "react";
+import zigzag from "../assets/zigzag-yellow-small.svg";
+import pentagon from "../assets/pentagon.svg";
 import topCurve from "../assets/candle-wallet-top-curve.svg";
+import Newsletter from "../components/Newsletter";
+import { RiGithubFill, RiGithubLine, RiInstagramLine, RiTwitterFill, RiTwitterLine } from "@remixicon/react";
 
 const Footer = () => {
   return (
-      <div className='bg-primary'>
-          <img className="w-full" src={topCurve} alt="" />
-    </div>
-  )
-}
+    <div className="bg-primary relative z-0 overflow-clip">
+      <img className="w-full" src={topCurve} alt="" />
+      <img
+        className="absolute w-56 md:w-80 -right-20 bottom-36 rotate-12 -z-10 [mask-image:linear-gradient(to_top,_black,_transparent)]"
+        src={pentagon}
+        alt=""
+      />
+      <div className="section-width py-10">
+        <Newsletter />
 
-export default Footer
+        <div className="flex flex-col md:flex-row gap-16 md:items-center mt-16 md:mt-8">
+          <div className="text-white">
+            <h2 className="text-3xl">EVM based scaling platform</h2>
+            <h1 className="text-5xl font-bold">Candle Chain</h1>
+          </div>
+          <img className="-mt-16 md:-mt-0 w-56" src={zigzag} alt="" />
+        </div>
+
+        <div className="text-white text-xl grid grid-cols-2 gap-10 md:grid-cols-4 w-[90%] mt-5">
+          <div>
+            <h2 className="font-bold mb-5">SOLUTIONS</h2>
+            <ul className="flex flex-col gap-2">
+              <li>Marketing</li>
+              <li>Analytics</li>
+              <li>Commerce</li>
+              <li>Insights</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="font-bold mb-5">SUPPORT</h2>
+            <ul className="flex flex-col gap-2">
+              <li>Pricing</li>
+              <li>Documentation</li>
+              <li>Guides</li>
+              <li>API Status</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="font-bold mb-5">COMPANY</h2>
+            <ul className="flex flex-col gap-2">
+              <li>About</li>
+              <li>Blog</li>
+              <li>Jobs</li>
+              <li>Press</li>
+              <li>Partners</li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="font-bold mb-5">LEGAL</h2>
+            <ul className="flex flex-col gap-2">
+              <li>Privacy</li>
+              <li>Terms</li>
+            </ul>
+          </div>
+        </div>
+
+        <hr className="mt-10 mb-5"/>
+
+        <div className="flex  flex-col md:flex-row items-center gap-5 justify-between">
+          <p className="text-white text-xl">© 2022 Candle Labs, Inc. All rights reserved.</p>
+          <ul className="flex text-white gap-4">
+            <li><RiInstagramLine /></li>
+            <li><RiTwitterFill /></li>
+            <li><RiGithubFill /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
